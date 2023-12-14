e1beb9670d074109ef2f96974f41b5db218176df
Lerato Seoke <seokemotlalepula@gmail.com>
2023-12-14 13:55:52 -0800

    Created the initial program

---
+++ shell.c
@@ -0,0 +1,11 @@
+/**
+ * main - program that is used to satisfy betty.
+ * Return: 0 (Success)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello Betty. Please Be proud");
+	return (0);
+}
